274898ad2d09f0f7ed7e2319fc09e8e0d4dde15e
Asgeir Nilsen <asgeir@fintlabs.no>
2021-08-31 13:34:25 +0200

    Proper size calculations according to k8s documentation.
    Allocates 90% of size minus 256MiB for heap.

--- src/main/java/no/fintlabs/operator/repository/RepositoryHelper.java
+++ src/main/java/no/fintlabs/operator/repository/RepositoryHelper.java
@@ -1,7 +1,12 @@
 package no.fintlabs.operator.repository;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RepositoryHelper {
 
@@ -14,9 +19,52 @@ public class RepositoryHelper {
     }
 
     public static String getXmx(String limit) {
-        String unit = limit.replaceAll("\\d+", "").replace("i", "");
-        double memoryLimit = Double.parseDouble(limit.replaceAll("[a-zA-Z]+", ""));
+        return parseSize(limit)
+                .multiply(new BigDecimal("0.9"))
+                .subtract(new BigDecimal("268435456"))
+                .divide(new BigDecimal("1048576"), RoundingMode.HALF_UP)
+                .toBigInteger() + "M";
+    }
 
-        return String.format("%d%s", Math.round((memoryLimit * 0.5) - 0.5), unit);
+    public static BigDecimal parseSize(String size) {
+        final Matcher matcher = Pattern.compile("([0-9.e]+)([EPTGMkK]i?)?").matcher(size);
+        if (matcher.matches()) {
+            return new BigDecimal(matcher.group(1)).multiply(new BigDecimal(parseUnit(matcher.group(2))));
+        }
+        throw new IllegalArgumentException(size);
+    }
+
+    public static BigInteger parseUnit(String unit) {
+        if (unit == null) {
+            return BigInteger.ONE;
+        }
+        switch (unit) {
+            default:
+                return BigInteger.ONE;
+            case "Ki":
+                return BigInteger.TWO.pow(10);
+            case "Mi":
+                return BigInteger.TWO.pow(20);
+            case "Gi":
+                return BigInteger.TWO.pow(30);
+            case "Ti":
+                return BigInteger.TWO.pow(40);
+            case "Pi":
+                return BigInteger.TWO.pow(50);
+            case "Ei":
+                return BigInteger.TWO.pow(60);
+            case "k":
+                return BigInteger.TEN.pow(3);
+            case "M":
+                return BigInteger.TEN.pow(6);
+            case "G":
+                return BigInteger.TEN.pow(9);
+            case "T":
+                return BigInteger.TEN.pow(12);
+            case "P":
+                return BigInteger.TEN.pow(15);
+            case "E":
+                return BigInteger.TEN.pow(18);
+        }
     }
 }
--- src/test/groovy/no/fintlabs/operator/repository/RepositoryHelperSpec.groovy
+++ src/test/groovy/no/fintlabs/operator/repository/RepositoryHelperSpec.groovy
@@ -5,14 +5,20 @@ import spock.lang.Specification
 
 class RepositoryHelperSpec extends Specification {
 
-    def "Converting memory limit to Xms should result in max 90% of limit"() {
+    def 'Can parse proper sizes as defined by k8s doc'() {
         expect:
-        RepositoryHelper.getXmx("32Gi") == "28G"
-
+        RepositoryHelper.parseSize('256Mi').toPlainString() == '268435456'
+        RepositoryHelper.parseSize('2Gi').toPlainString() == '2147483648'
+        RepositoryHelper.parseSize('2G').toPlainString() == '2000000000'
+        RepositoryHelper.parseSize('1e6').toPlainString() == '1000000'
+        RepositoryHelper.parseSize('2.88').toPlainString() == '2.88'
+        RepositoryHelper.parseSize('2.88Gi').toPlainString() == '3092376453.12'
     }
 
-    def 'Converting megabyte memory limit should also result in 90% of limit'() {
+    def 'Proper heap size calculations'() {
         expect:
-        RepositoryHelper.getXmx('256Mi') == '230M'
+        RepositoryHelper.getXmx("512Mi") == '204M'
+        RepositoryHelper.getXmx("2Gi") == '1587M'
+        RepositoryHelper.getXmx("18Gi") == '16332M'
     }
 }
