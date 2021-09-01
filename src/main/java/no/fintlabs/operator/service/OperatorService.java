99b546e3809a7e601682b4d78d58ba3a407ae34c
Frode Sjovatsen <frode@fintlabs.no>
2021-09-01 21:50:10 +0200

    FC-56 #comment Refactoring #in-progress

--- src/main/java/no/fintlabs/operator/service/OperatorService.java
+++ src/main/java/no/fintlabs/operator/service/OperatorService.java
@@ -1,6 +1,5 @@
 package no.fintlabs.operator.service;
 
-import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.operator.configuration.AppConfiguration;
@@ -33,82 +32,83 @@ public class OperatorService {
         this.configuration = configuration;
     }
 
-    @PostConstruct
-    public void init() {
-        updateDeployments();
-    }
+//    @PostConstruct
+//    public void init() {
+//        updateConsumers();
+//    }
 
-    @Scheduled(fixedRate = 30000L)
+    @Scheduled(fixedRate = 30000L, initialDelayString = "${}")
     public void refreshConsumers() {
-        updateDeployments();
+        updateConsumers();
     }
 
-    public void updateDeployments() {
-        final List<FintConsumerDefinition> fintConsumers = Objects.requireNonNull(webClient.get()
+    public void updateConsumers() {
+        final List<FintConsumerDefinition> fintConsumers = getFintConsumerDefinitions();
+        log.info("There are {} FINT consumers defined.", fintConsumers.size());
+
+        updateServices(fintConsumers);
+        updateDeployments(fintConsumers);
+        deleteUnusedDeployments(fintConsumers);
+        deleteUnusedServices(fintConsumers);
+
+    }
+
+    private List<FintConsumerDefinition> getFintConsumerDefinitions() {
+        return Objects.requireNonNull(webClient.get()
                         .retrieve()
                         .bodyToMono(new ParameterizedTypeReference<List<K8sDeploymentModel>>() {
                         })
                         .block())
                 .stream()
                 .flatMap(fintConsumerService::createFintConsumers)
-                .peek(it -> log.info("-> {} [{}]", it.getName(), it.getOrgId()))
+                //.peek(it -> log.info("-> {} [{}]", it.getName(), it.getOrgId()))
                 .collect(Collectors.toList());
-        log.info("There are {} FINT consumers defined.", fintConsumers.size());
+    }
 
-        fintConsumers
-                .stream()
-                .map(FintConsumerDefinition::getService)
-                .forEach(service -> {
-                    log.info("-> Updating service {}", service.getMetadata().getName());
-                    client.services().inNamespace(configuration.getNamespace()).createOrReplace(service);
-                });
-
-        //log.info("Updated {} services.", fintConsumers.size());
-
+    private void updateDeployments(List<FintConsumerDefinition> fintConsumers) {
+        log.info("Updating deployments...");
         fintConsumers
                 .stream()
                 .map(FintConsumerDefinition::getDeployment)
                 .forEach(deployment -> {
-                    log.info("-> Updating deployment {}", deployment.getMetadata().getName());
+                    log.info("-> Updating {}", deployment.getMetadata().getName());
                     client.apps().deployments().inNamespace(configuration.getNamespace()).createOrReplace(deployment);
                 });
+    }
 
+    private void updateServices(List<FintConsumerDefinition> fintConsumers) {
+        log.info("Updating services...");
+        fintConsumers
+                .stream()
+                .map(FintConsumerDefinition::getService)
+                .forEach(service -> {
+                    log.info("-> Updating {}", service.getMetadata().getName());
+                    client.services().inNamespace(configuration.getNamespace()).createOrReplace(service);
+                });
+    }
 
-        //log.info("Updated {} deployments.", deployments);
-
-        List<Deployment> deploymentsToDelete = client.apps().deployments()
+    private void deleteUnusedDeployments(List<FintConsumerDefinition> fintConsumers) {
+        client.apps().deployments().delete(client.apps().deployments()
                 .inNamespace(configuration.getNamespace())
                 .withLabels(Collections.singletonMap("fint.created-by", "fint-core-k8s-operator"))
                 .list()
                 .getItems()
                 .stream()
                 .filter(deployment -> fintConsumers.stream().noneMatch(fc -> fc.getName().equals(deployment.getMetadata().getName())))
-                .collect(Collectors.toList());
+                .peek(deployment -> log.info("-> Deleting deployment {}", deployment.getMetadata().getName()))
+                .collect(Collectors.toList()));
+    }
 
-//        List<Deployment> consumerDeployment = deployments.stream()
-//                .filter(deployment -> fintConsumers.stream().noneMatch(fc -> fc.getName().equals(deployment.getMetadata().getName())))
-//                .collect(Collectors.toList());
-
-        log.info("Deleting {} deployments", deploymentsToDelete.size());
-        client.apps().deployments().delete(deploymentsToDelete);
-
-
-        List<io.fabric8.kubernetes.api.model.Service> servicesToDelete = client.services()
+    private void deleteUnusedServices(List<FintConsumerDefinition> fintConsumers) {
+        client.services().delete(client.services()
                 .inNamespace(configuration.getNamespace())
                 .withLabels(Collections.singletonMap("fint.created-by", "fint-core-k8s-operator"))
                 .list()
                 .getItems()
                 .stream()
                 .filter(service -> fintConsumers.stream().noneMatch(fc -> fc.getName().equals(service.getMetadata().getName())))
-                .collect(Collectors.toList());
-
-//        List<io.fabric8.kubernetes.api.model.Service> consumerServices = items.stream()
-//                .filter(service -> fintConsumers.stream().noneMatch(fc -> fc.getName().equals(service.getMetadata().getName())))
-//                .collect(Collectors.toList());
-
-        log.info("Deleting {} services", servicesToDelete.size());
-        client.services().delete(servicesToDelete);
-
+                .peek(service -> log.info("-> Deleting service {}", service.getMetadata().getName()))
+                .collect(Collectors.toList()));
     }
 
 
