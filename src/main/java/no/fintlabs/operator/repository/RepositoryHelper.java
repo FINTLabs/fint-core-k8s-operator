627bfaa28222d8f989811d70079a9f6c7d997da7
Asgeir Storesund Nilsen <asgeir@fintlabs.no>
2021-09-02 12:28:59 +0200

    Proper size calculations according to k8s documentation. (#2)
    * Proper size calculations according to k8s documentation.
    
    Allocates 90% of size minus 256MiB for heap.
    
    * Extract constants.
    
    Co-authored-by: Frode Sjovatsen <fsjovatsen@users.noreply.github.com>

--- src/main/java/no/fintlabs/operator/repository/RepositoryHelper.java
+++ src/main/java/no/fintlabs/operator/repository/RepositoryHelper.java
@@ -1,9 +1,16 @@
 package no.fintlabs.operator.repository;
 
-import java.util.HashMap;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RepositoryHelper {
+
+    public static final BigDecimal SIZE_256_MB = new BigDecimal("268435456");
+    public static final BigDecimal SIZE_1_MB = new BigDecimal("1048576");
 
     public static Map<String, String> getLabels(String orgId, String component) {
         Map<String, String> selectors = getSelectors(orgId, component);
@@ -20,9 +27,52 @@ public class RepositoryHelper {
     }
 
     public static String getXmx(String limit) {
-        String unit = limit.replaceAll("\\d+", "").replace("i", "");
-        double memoryLimit = Double.parseDouble(limit.replaceAll("[a-zA-Z]+", ""));
+        return parseSize(limit)
+                .multiply(new BigDecimal("0.9"))
+                .subtract(SIZE_256_MB)
+                .divide(SIZE_1_MB, RoundingMode.HALF_UP)
+                .toBigInteger() + "M";
+    }
 
-        return String.format("%d%s", Math.round((memoryLimit * 0.5) - 0.5), unit);
+    public static BigDecimal parseSize(String size) {
+        final Matcher matcher = Pattern.compile("([0-9.e]+)([EPTGMkK]i?)?").matcher(size);
+        if (matcher.matches()) {
+            return new BigDecimal(matcher.group(1)).multiply(new BigDecimal(parseUnit(matcher.group(2))));
+        }
+        throw new IllegalArgumentException(size);
+    }
+
+    public static BigInteger parseUnit(String unit) {
+        if (unit == null) {
+            return BigInteger.ONE;
+        }
+        switch (unit) {
+            default:
+                return BigInteger.ONE;
+            case "Ki":
+                return BigInteger.TWO.pow(10);
+            case "Mi":
+                return BigInteger.TWO.pow(20);
+            case "Gi":
+                return BigInteger.TWO.pow(30);
+            case "Ti":
+                return BigInteger.TWO.pow(40);
+            case "Pi":
+                return BigInteger.TWO.pow(50);
+            case "Ei":
+                return BigInteger.TWO.pow(60);
+            case "k":
+                return BigInteger.TEN.pow(3);
+            case "M":
+                return BigInteger.TEN.pow(6);
+            case "G":
+                return BigInteger.TEN.pow(9);
+            case "T":
+                return BigInteger.TEN.pow(12);
+            case "P":
+                return BigInteger.TEN.pow(15);
+            case "E":
+                return BigInteger.TEN.pow(18);
+        }
     }
 }
