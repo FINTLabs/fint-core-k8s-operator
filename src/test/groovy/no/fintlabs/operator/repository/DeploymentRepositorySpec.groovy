5b3a9ce08d58aa2f22d5d272633812661305d1f6
Frode Sjovatsen <frode@fintlabs.no>
2021-09-02 19:00:00 +0200

    FC-56 #comment Cleanup #in-progress

--- src/test/groovy/no/fintlabs/operator/repository/DeploymentRepositorySpec.groovy
+++ src/test/groovy/no/fintlabs/operator/repository/DeploymentRepositorySpec.groovy
@@ -1,6 +1,6 @@
 package no.fintlabs.operator.repository
 
-import io.fabric8.kubernetes.api.model.Quantity
+
 import spock.lang.Specification
 
 class DeploymentRepositorySpec extends Specification {
